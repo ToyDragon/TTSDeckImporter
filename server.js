6d70e97b5a9a444e0338f3df70dc6518fe659e34
Matt Bates <BatesMatthewJ@GMail.com>
2016-02-28 08:49:14 -0600

    Queue up requests and handle them serially

--- server.js
+++ server.js
@@ -24,6 +24,17 @@ var draftsToday = {
 	//}
 };
 
+var handleingRequest = false;
+var requestQueue = [
+	//{
+	//	req: request,
+	//	res: response,
+	//	isDraft: bool
+	//}
+];
+
+var app = express();
+
 function prettifyJson(msg){
 	msg = msg.replace(/,/g,',\r\n').replace(/([\[{])/g,'$1\r\n').replace(/([\]}])/g,'\r\n$1');
 	var lines = msg.split('\r\n');
@@ -116,7 +127,7 @@ function notifyDeck(){
 			console.log(json);
 		});
 		lastErrorEmail = new Date();
-		lastErrorEmail.setMinutes(lastErrorEmail.getMinutes() + 30);
+		lastErrorEmail.setMinutes(lastErrorEmail.getMinutes() + 150);
 
 		lastDailySummaryEmail = new Date();
 		lastDailySummaryEmail.setDate(lastDailySummaryEmail.getDate()+1);
@@ -218,7 +229,114 @@ function majorError(details){
 	}
 };
 
-var app = express();
+function HandleRequest(){
+	if(!handleingRequest && requestQueue.length > 0){
+		handleingRequest = true;
+
+		var reqObj = requestQueue.shift();
+		if(reqObj && reqObj.req && reqObj.res){
+			if(reqObj.isDraft) HandleDraft(reqObj.req, reqObj.res);
+			else HandleDeck(reqObj.req, reqObj.res);
+		}
+	}
+};
+
+function HandleDraft(req, res){
+	var client = net.connect({port: config.port});
+	var deckId = req.body.set.replace(/[^a-zA-Z0-9]/g, '') + '_' + getDeckID();
+
+	client.on('error', function(){
+		console.log('Deck maker is down...');
+		majorError({'message': 'Unable to connect to deck maker'});
+		res.end(JSON.stringify({
+			status:1,
+			errObj: {
+				message: 'The server is experiencing technical issues, please check back soon for details.'
+			}
+		}));
+		handleingRequest = false;
+		HandleRequest();
+	});
+
+	client.on('close', function(){
+		res.end(JSON.stringify({name:deckId,status:0}));
+		handleingRequest = false;
+		HandleRequest();
+	});
+
+	client.write('draft\r\n');
+	client.write(deckId + '\r\n');
+	client.write(cleanNewLines(req.body.set) + '\r\n');
+	client.write(clean(req.body.n) + '\r\n');
+	
+	logDraft(req.body);
+};
+
+function HandleDeck(req, res){
+	var decklist = req.body.decklist + '\r\nENDDECK';
+	var deckID = getDeckID();
+	
+	var backURL = clean(req.body.backURL);
+	var hiddenURL = clean(req.body.hiddenURL);
+	var compression = clean(req.body.compression);
+	var deckName = cleanNewLines(req.body.name);
+	var useImgur = !!req.body.imgur;
+	var coolifyBasic = !!req.body.coolify;
+
+	var client = net.connect({port: config.port});
+	
+	client.on('error', function(){
+		console.log('Deck maker is down...');
+		majorError({'message': 'Unable to connect to deck maker'});
+		res.end(JSON.stringify({
+			status:1,
+			errObj: {
+				message: 'The server is experiencing technical issues, please check back soon for details.'
+			}
+		}));
+		handleingRequest = false;
+		HandleRequest();
+	});
+
+	client.on('close', function(){
+		var errObj = null;
+		try{
+			errObj = JSON.parse(data)
+		}catch(err){}
+		if(errObj){
+			console.log(errObj);
+			res.end(JSON.stringify({
+				status:1,
+				errObj: errObj
+			}));
+		}else{
+			res.end(JSON.stringify({
+				status: 0,
+				name: deckID
+			}));
+		}
+		logDeck(req.body, !!errObj);
+		handleingRequest = false;
+		HandleRequest();
+	});
+
+	var data = '';
+	var decoder = new StringDecoder('utf8');
+	client.on('data', function(buffer){
+		data += decoder.write(buffer);
+	});
+
+	client.write('deck\r\n');
+	client.write(deckID + '\r\n');
+	client.write(deckName + '\r\n');
+	client.write(useImgur + '\r\n');
+	client.write(backURL + '\r\n');
+	client.write(hiddenURL + '\r\n');
+	client.write(coolifyBasic + '\r\n');
+	client.write(compression + '\r\n');
+	client.write(decklist + '\r\n');
+	client.write('ENDDECK\r\n');
+}
 
 app.use(express.static('www'));
 app.use('/decks', express.static(config.deckDir));
@@ -242,84 +360,26 @@ app.get('/sets', function(req, res){
 
 app.post('/newdraft', function(req, res){
 	try{
-		var client = net.connect({port: config.port});
-		var deckId = req.body.set.replace(/[^a-zA-Z0-9]/g, '') + '_' + getDeckID();
-		client.on('close', function(){
-			res.end(JSON.stringify({name:deckId,status:0}));
+		requestQueue.push({
+			req: req,
+			res: res,
+			isDraft: true
 		});
-
-		client.write('draft\r\n');
-		client.write(deckId + '\r\n');
-		client.write(cleanNewLines(req.body.set) + '\r\n');
-		client.write(clean(req.body.n) + '\r\n');
-		
-		logDraft(req.body);
+		HandleRequest();
 	}catch(err){
+		console.log(err);
 		res.end(JSON.stringify({status:1,errObj:{message:'Unknown error occured'}}));
 	}
 });
 
 app.post('/newdeck', function(req, res){
 	try{
-		var decklist = req.body.decklist + '\r\nENDDECK';
-		var deckID = getDeckID();
-		
-		var backURL = clean(req.body.backURL);
-		var hiddenURL = clean(req.body.hiddenURL);
-		var compression = clean(req.body.compression);
-		var deckName = cleanNewLines(req.body.name);
-		var useImgur = !!req.body.imgur;
-		var coolifyBasic = !!req.body.coolify;
-
-		var client = net.connect({port: config.port});
-		
-		client.on('error', function(){
-			console.log('Deck maker is down...');
-			majorError({'message': 'Unable to connect to deck maker'});
-			res.end(JSON.stringify({
-				status:1,
-				errObj: {
-					message: 'The server is experiencing technical issues, please check back soon for details.'
-				}
-			}));
-		})
-
-		client.on('close', function(){
-			var errObj = null;
-			try{
-				errObj = JSON.parse(data)
-			}catch(err){}
-			if(errObj){
-				console.log(errObj);
-				res.end(JSON.stringify({
-					status:1,
-					errObj: errObj
-				}));
-			}else{
-				res.end(JSON.stringify({
-					status: 0,
-					name: deckID
-				}));
-			}
-			logDeck(req.body, !!errObj);
+		requestQueue.push({
+			req: req,
+			res: res,
+			isDraft: false
 		});
-
-		var data = '';
-		var decoder = new StringDecoder('utf8');
-		client.on('data', function(buffer){
-			data += decoder.write(buffer);
-		});
-
-		client.write('deck\r\n');
-		client.write(deckID + '\r\n');
-		client.write(deckName + '\r\n');
-		client.write(useImgur + '\r\n');
-		client.write(backURL + '\r\n');
-		client.write(hiddenURL + '\r\n');
-		client.write(coolifyBasic + '\r\n');
-		client.write(compression + '\r\n');
-		client.write(decklist + '\r\n');
-		client.write('ENDDECK\r\n');
+		HandleRequest();
 	}catch(err){
 		console.log(err);
 		res.end(JSON.stringify({status:1,errObj:{message:'Unknown error occured'}}));
