4c264d668256279cc7b5fd97f7b2dc85000ac1e3
Matt Bates <BatesMatthewJ@GMail.com>
2016-06-17 17:13:25 -0500

    output filename on failure

--- libs/src/utils/cardretrieval/MagicCardsInfoRetriever.java
+++ libs/src/utils/cardretrieval/MagicCardsInfoRetriever.java
@@ -23,12 +23,15 @@ public class MagicCardsInfoRetriever extends CardRetriever{
 	public boolean LoadCard(Card card, boolean isBack){
 		String cardKey = isBack ? card.transformCardKey : card.cardKey;
 		String cardName = isBack ? card.transformName : card.name;
-		String imageFileName = Config.imageDir+cardKey.toLowerCase().trim().replaceAll("[<>/\"]+", "_")+".jpg";
+		String imageFileName = Config.imageDir+cardKey.toLowerCase().trim().replaceAll("[/\"]+", "_")+".jpg";
 		
 		if(isBack) card.transformImageFileName = imageFileName;
 		else card.imageFileName = imageFileName;
 
-		if(new File(imageFileName).exists()) return true;
+		if(new File(imageFileName).exists()){
+			System.out.println("Read from \""+imageFileName+"\"");
+			return true;
+		}
 		if(HandleHardCard(cardName, imageFileName)) return true;
 		
 		String result = RequestCard(card, cardName);
